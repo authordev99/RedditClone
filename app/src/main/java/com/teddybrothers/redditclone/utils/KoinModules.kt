9ea1ddf6468feb809cda1a212d845cb97343ec6a
Federico <federico@selfdrvn.com>
2020-10-06 17:42:36 +0800

    Using RedditCloneApplication.kt in AndroidManifest.xml Remove context dependency in app module koin

--- app/src/main/java/com/teddybrothers/redditclone/utils/KoinModules.kt
+++ app/src/main/java/com/teddybrothers/redditclone/utils/KoinModules.kt
@@ -1,14 +1,10 @@
 package com.teddybrothers.redditclone.utils
 
 import com.teddybrothers.redditclone.viewmodels.TopicViewModel
-import org.koin.android.ext.koin.androidContext
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     viewModel { TopicViewModel(get()) }
-    single {
-       SessionManager(get())
-    }
-    single { androidContext() }
+    single { SessionManager(get()) }
 }

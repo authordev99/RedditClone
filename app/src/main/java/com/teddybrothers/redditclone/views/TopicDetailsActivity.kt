cc6828c483f84fac9ebfa50b6313eb5bd133fd51
Federico <federico@selfdrvn.com>
2020-10-07 15:04:27 +0800

    Passing back topic model to MainActivity.kt and update to recyclerview

--- app/src/main/java/com/teddybrothers/redditclone/views/TopicDetailsActivity.kt
+++ app/src/main/java/com/teddybrothers/redditclone/views/TopicDetailsActivity.kt
@@ -1,5 +1,7 @@
 package com.teddybrothers.redditclone.views
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -8,6 +10,7 @@ import com.teddybrothers.redditclone.R
 import com.teddybrothers.redditclone.databinding.ActivityTopicDetailsBinding
 import com.teddybrothers.redditclone.models.Topic
 import com.teddybrothers.redditclone.viewmodels.TopicViewModel
+import com.teddybrothers.redditclone.views.MainActivity.Companion.PARAM_TOPIC
 import org.koin.android.ext.android.inject
 
 class TopicDetailsActivity : AppCompatActivity() {
@@ -26,5 +29,13 @@ class TopicDetailsActivity : AppCompatActivity() {
 
         binding.topic = topic
         binding.topicViewModel = topicViewModel
+    }
+
+    override fun onBackPressed() {
+        val returnIntent = Intent().apply {
+            putExtra(PARAM_TOPIC, Gson().toJson(binding.topic))
+        }
+        setResult(Activity.RESULT_OK,returnIntent)
+        finish()
     }
 }

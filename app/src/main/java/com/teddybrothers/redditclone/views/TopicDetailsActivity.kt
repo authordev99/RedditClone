8f242d49c9aece08611b297aecd23db6cf7cf095
Federico <federico@selfdrvn.com>
2020-10-07 17:05:43 +0800

    init toolbar and action close for back button

--- app/src/main/java/com/teddybrothers/redditclone/views/TopicDetailsActivity.kt
+++ app/src/main/java/com/teddybrothers/redditclone/views/TopicDetailsActivity.kt
@@ -3,6 +3,7 @@ package com.teddybrothers.redditclone.views
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.gson.Gson
@@ -11,6 +12,7 @@ import com.teddybrothers.redditclone.databinding.ActivityTopicDetailsBinding
 import com.teddybrothers.redditclone.models.Topic
 import com.teddybrothers.redditclone.viewmodels.TopicViewModel
 import com.teddybrothers.redditclone.views.MainActivity.Companion.PARAM_TOPIC
+import kotlinx.android.synthetic.main.toolbar.*
 import org.koin.android.ext.android.inject
 
 class TopicDetailsActivity : AppCompatActivity() {
@@ -24,18 +26,38 @@ class TopicDetailsActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_topic_details)
 
         intent.extras?.apply {
-            topic = Gson().fromJson(getString(MainActivity.PARAM_TOPIC), Topic::class.java)
+            topic = Gson().fromJson(getString(PARAM_TOPIC), Topic::class.java)
         }
 
         binding.topic = topic
         binding.topicViewModel = topicViewModel
+
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeButtonEnabled(true)
+            title = ""
+        }
     }
 
     override fun onBackPressed() {
         val returnIntent = Intent().apply {
             putExtra(PARAM_TOPIC, Gson().toJson(binding.topic))
         }
-        setResult(Activity.RESULT_OK,returnIntent)
+        setResult(Activity.RESULT_OK, returnIntent)
         finish()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val itemId = item.itemId
+        if (itemId == android.R.id.home) // Press Back Icon
+        {
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

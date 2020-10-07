98a255aa1f9dc4117230ea972b988c24137f8b57
Federico <federico@selfdrvn.com>
2020-10-07 14:33:15 +0800

    - Received topic from MainActivity.kt as intent extra and binding it to UI - Binding TopicViewModel.kt to UI

--- app/src/main/java/com/teddybrothers/redditclone/views/TopicDetailsActivity.kt
+++ app/src/main/java/com/teddybrothers/redditclone/views/TopicDetailsActivity.kt
@@ -1,12 +1,30 @@
 package com.teddybrothers.redditclone.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.gson.Gson
 import com.teddybrothers.redditclone.R
+import com.teddybrothers.redditclone.databinding.ActivityTopicDetailsBinding
+import com.teddybrothers.redditclone.models.Topic
+import com.teddybrothers.redditclone.viewmodels.TopicViewModel
+import org.koin.android.ext.android.inject
 
 class TopicDetailsActivity : AppCompatActivity() {
+
+    lateinit var topic: Topic
+    lateinit var binding: ActivityTopicDetailsBinding
+    private val topicViewModel by inject<TopicViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_topic_details)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_topic_details)
+
+        intent.extras?.apply {
+            topic = Gson().fromJson(getString(MainActivity.PARAM_TOPIC), Topic::class.java)
+        }
+
+        binding.topic = topic
+        binding.topicViewModel = topicViewModel
     }
 }
